605d6663ee23dc2a8f4af98002a47ccc242c1132
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2022-11-22 16:05:01 -0300

    Agregar archivos de proyecto.

---
+++ Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alacena
+{
+    internal class Producto
+    {
+        internal int id;
+        internal string nombre;
+        internal string marca;
+        internal int cantidad;
+        internal DateTime fechaDeVencimiento;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alacena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            List<Producto> productos = new List<Producto>();
+            do
+            {
+                MostrarProductos();
+                Console.WriteLine();
+                Console.WriteLine("1- Agregar producto");
+                Console.WriteLine("2- Eliminar producto");
+                Console.WriteLine("3- Mostrar productos vencidos");
+                Console.WriteLine("4- Exportar productos vencidos");
+                Console.WriteLine("0- Salir");
+                Console.Write("\nSeleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Producto nuevoProducto = new Producto();
+                        if(productos.Any())
+                            nuevoProducto.id = productos.Max(p => p.id) + 1;
+                        else
+                            nuevoProducto.id = 1;
+                        Console.Write("Nombre: ");
+                        nuevoProducto.nombre = Console.ReadLine();
+                        Console.Write("Marca: ");
+                        nuevoProducto.marca = Console.ReadLine();
+                        Console.Write("Cantidad: ");
+                        nuevoProducto.cantidad = int.Parse(Console.ReadLine());
+                        Console.Write("Fecha de vencimiento (DD/MM/YY): ");
+                        nuevoProducto.fechaDeVencimiento = DateTime.Parse(Console.ReadLine());
+                        productos.Add(nuevoProducto);
+                        break;
+                    case 2:
+                        // Implementar
+                        break;
+                    case 3:
+                        // Implementar
+                        break;
+                    case 4:
+                        // Implementar
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcion != 0);
+
+            void MostrarProductos()
+            {
+                foreach (Producto producto in productos)
+                {
+                    Console.Write("ID: " + producto.id);
+                    Console.Write(" - " + producto.nombre);
+                    Console.Write(" " + producto.marca);
+                    Console.Write(" (" + producto.cantidad + ")");
+                    Console.WriteLine(" - " + producto.fechaDeVencimiento.ToShortDateString());                    
+                }                
+            }
+        }
+    }
+}
